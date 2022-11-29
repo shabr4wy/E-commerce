103cd25c137f320ee32de12a25aa2d778636dffe
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-11-29 20:53:12 +0200

    feat: add nav component

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,7 +1,21 @@
 /** @format */
 
 const Nav = () => {
-  return <nav className="nav"></nav>;
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          <a>ALl Products</a>
+        </li>
+        <li>
+          <details>
+            <summary>Categories</summary>
+            <ul></ul>
+          </details>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
