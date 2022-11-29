a36907a32d0de9ecbbbeb6ac453cd5fd47541ca1
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-11-29 21:41:58 +0200

    feat: fetch products categories

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,19 @@
 /** @format */
 
+import { useEffect, useState } from "react";
+
 const Nav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = () => {
+      fetch("https://dummyjson.com/products/categories")
+        .then((res) => res.json())
+        .then((categories) => setCategories([...categories]));
+    };
+    getCategories();
+  }, [categories]);
+
   return (
     <nav className="nav">
       <ul>
