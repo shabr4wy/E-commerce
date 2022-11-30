fed2dafeffe42e632fc01823c41eea24e805b7c2
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-11-30 19:56:51 +0200

    feat: fetch products data

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -1,6 +1,21 @@
 /** @format */
 
+import { useEffect, useState } from "react";
+
 const AllProdcucts = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = () => {
+      fetch("https://dummyjson.com/products?limit=10")
+        .then((res) => res.json())
+        .then((data) => {
+          setProducts([...data.products]);
+        });
+    };
+    getProducts();
+  }, [products]);
+
   return <div className="main__products"></div>;
 };
 
