1cfcf9faeded2566e7883f0770d7c6fe2d8211a7
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-12-03 21:45:55 +0200

    feat: create root rout

--- src/Index.jsx
+++ src/Index.jsx
@@ -2,11 +2,19 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import Root from "./routes/Root";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
