dd3999f40b078925aec0d9bee09bdccfc806a6c9
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-12-03 21:39:33 +0200

    refactor: set root route component

---
+++ src/routes/Root.jsx
@@ -0,0 +1,16 @@
+/** @format */
+import Header from "../components/Header";
+import AllProducts from "../components/AllProducts";
+import Nav from "../components/Nav";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <AllProducts />
+    </div>
+  );
+};
+
+export default App;
