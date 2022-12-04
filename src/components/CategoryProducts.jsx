445a54ee0649def8c9f4ca7f6a3245ca172bfc3c
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-12-04 21:00:55 +0200

    feat: fetch category products depending on param

--- src/components/CategoryProducts.jsx
+++ src/components/CategoryProducts.jsx
@@ -1,10 +1,24 @@
 /** @format */
 
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import Products from "./Products";
 
 const CategoryProducts = () => {
   const [categoryProducts, setCategoryProducts] = useState([]);
+  let params = useParams();
+
+  // fetch category products depending on param value "category name"
+  useEffect(() => {
+    const getCategoryProducts = () => {
+      fetch(`https://dummyjson.com/products/category/${params.categoryName}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setCategoryProducts([...data.products]);
+        });
+    };
+    getCategoryProducts();
+  }, [params.categoryName]);
 
   return <Products products={categoryProducts} />;
 };
