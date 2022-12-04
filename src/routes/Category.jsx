78fdef05921fcebe34be25cd27ead586a0764dda
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-12-04 20:38:13 +0200

    feat: create category component

---
+++ src/routes/Category.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import CategoryProducts from "../components/CategoryProducts";
+
+const Category = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <CategoryProducts />
+    </>
+  );
+};
+
+export default Category;
