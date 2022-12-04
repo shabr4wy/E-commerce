eefc4fde909473e57dbc74aa85928c54d765c63a
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-12-04 21:09:36 +0200

    abort fetch to cleanUp useEffect

--- src/components/CategoryProducts.jsx
+++ src/components/CategoryProducts.jsx
@@ -10,14 +10,25 @@ const CategoryProducts = () => {
 
   // fetch category products depending on param value "category name"
   useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     const getCategoryProducts = () => {
-      fetch(`https://dummyjson.com/products/category/${params.categoryName}`)
+      fetch(
+        `https://dummyjson.com/products/category/${params.categoryName}`,
+        signal
+      )
         .then((res) => res.json())
         .then((data) => {
           setCategoryProducts([...data.products]);
         });
     };
     getCategoryProducts();
+
+    return () => {
+      // cancel the request before component unmounts
+      controller.abort();
+    };
   }, [params.categoryName]);
 
   return <Products products={categoryProducts} />;
