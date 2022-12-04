45b980041bb656de9c35e8f28c39479179117e64
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-12-04 21:15:53 +0200

    feat: cleanup useEffect of all products component

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -6,15 +6,25 @@ import { useEffect, useState } from "react";
 const AllProdcucts = () => {
   const [products, setProducts] = useState([]);
 
+  // fetch products
   useEffect(() => {
-    const getProducts = () => {
-      fetch("https://dummyjson.com/products?limit=10")
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const getCategoryProducts = () => {
+      fetch("https://dummyjson.com/products?limit=10", signal)
         .then((res) => res.json())
         .then((data) => {
-          setProducts([...data?.products]);
+          setProducts([...data.products]);
         });
     };
-    getProducts();
+    getCategoryProducts();
+
+    return () => {
+      // cancel the request before component unmounts
+      controller.abort();
+      console.log(signal);
+    };
   }, []);
 
   return <Products products={products} />;
