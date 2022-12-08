a3185b8608c67c4d10ba46026175480969faa817
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-12-08 16:51:45 +0200

    refactor: fetch categoryProducts using useQuery

--- src/components/CategoryProducts.jsx
+++ src/components/CategoryProducts.jsx
@@ -1,40 +1,29 @@
 /** @format */
 
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 
 const CategoryProducts = () => {
-  const [categoryProducts, setCategoryProducts] = useState([]);
   let params = useParams();
 
   // fetch category products depending on param value "category name"
-  useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
+  const getCategoryProducts = async () => {
+    const res = await fetch(
+      `https://dummyjson.com/products/category/${params.categoryName}`
+    );
+    return res.json();
+  };
 
-    const getCategoryProducts = () => {
-      fetch(
-        `https://dummyjson.com/products/category/${params.categoryName}`,
-        signal
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          setCategoryProducts([...data.products]);
-        });
-    };
-    getCategoryProducts();
-
-    return () => {
-      // cancel the request before component unmounts
-      controller.abort();
-    };
-  }, [params.categoryName]);
+  const { data, status } = useQuery(
+    ["categoryProducts", params],
+    getCategoryProducts
+  );
 
   return (
     <main className="main">
       <div className="main__products">
         <ul>
-          {categoryProducts?.map((product) => (
+          {data?.products?.map((product) => (
             <li key={product?.title}>
               <a href="">{product?.title}</a>
             </li>
