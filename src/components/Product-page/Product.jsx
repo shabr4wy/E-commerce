10205ab6355057693b9d320ae5f1bfd5f9639a10
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2022-12-12 20:35:20 +0200

    feat: fetch product data using product id

--- src/components/Product-page/Product.jsx
+++ src/components/Product-page/Product.jsx
@@ -1,13 +1,24 @@
 /** @format */
 
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 
 const ProductPage = () => {
   let params = useParams();
 
+  // fetch product data using product id
+  const getProduct = async () => {
+    const res = await fetch(
+      `https://dummyjson.com/products/${params.productId}`
+    );
+    return res.json();
+  };
+
+  const { data, status } = useQuery("product", getProduct);
+
   return (
     <main>
-      <p>{data?.productName}</p>
+      <p>{data?.title}</p>
     </main>
   );
 };
